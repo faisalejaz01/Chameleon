474459131ea18ebb7e69a24fdfe92e929b80fe47
Justin Phlegar <phlegarj85@yahoo.com>
2018-03-26 14:43:28 -0400

    Sandbox merge

--- src/test/java/com/orasi/Sandbox.java
+++ src/test/java/com/orasi/Sandbox.java
@@ -1,24 +1,45 @@
 package com.orasi;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.orasi.utils.io.FileLoader;
 import com.orasi.web.OrasiDriver;
 import com.orasi.web.WebBaseTest;
 
 public class Sandbox extends WebBaseTest {
 
-    @BeforeMethod
+    // @BeforeMethod
     public void setup() {
         DriverManagerFactory.getManager(DriverType.FIREFOX).initalizeDriver();
     }
 
     @Test
     public void getAllSelectedOptions() {
-        launchSite();
-        searchForOrasi();
+        File tempDir = new File("/tmp");
+        tempDir.mkdir();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("/tmp/emails.txt"));) {
+            writer.write("adam.thomas@orasi.com");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        String emails = "";
+        try {
+            emails = FileLoader.loadFileFromProjectAsString("/tmp/emails.txt");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(emails);
     }
 
     public void launchSite() {
