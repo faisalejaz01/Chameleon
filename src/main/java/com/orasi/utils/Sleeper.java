2660db3e48389cc67327d3fcb513e747118283ce
Zach Apple <zapple94@gmail.com>
2018-04-22 19:57:23 -0400

    Change Sleeper.sleep and all references to use seconds

--- src/main/java/com/orasi/utils/Sleeper.java
+++ src/main/java/com/orasi/utils/Sleeper.java
@@ -1,15 +1,19 @@
 package com.orasi.utils;
 
 import org.apache.commons.lang3.time.StopWatch;
+import static com.orasi.utils.Constants.MAX_SLEEP_TIME;
 
 public class Sleeper {
-    public static void sleep(long millis) {
-        TestReporter.logTrace("Sleeping for [ " + millis + " ] milliseconds");
+    public static void sleep(double seconds) {
+    	if (seconds>MAX_SLEEP_TIME) {
+    		throw new RuntimeException("Sleep time exceeds 600 seconds.");
+    	}
+        TestReporter.logTrace("Sleeping for [ " + seconds + " ] seconds");
         StopWatch stopwatch = new StopWatch();
         stopwatch.start();
         do {
 
-        } while (stopwatch.getTime() < millis);
+        } while ((double)(stopwatch.getTime()/1000.0) < seconds);
         stopwatch.stop();
         stopwatch.reset();
     }
